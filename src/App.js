8083395258a0de3e0635f139f551060796b529f5
aarthipriya2021 <a.aarthipriya2021@gmail.com>
2021-07-23 00:46:56 +0530

    usereducer hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, { useReducer} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const initialState = { width: 15 }
+const reducer = (state, newState) => ({
+  ...state,
+  width: newState.width
+})
+
+const Bar = () => {
+  const [state, setState] = useReducer(reducer, initialState)
+  return <>
+    <div style={{ background: 'teal', height: '30px', width: state.width }}></div>
+    <div style={{marginTop: '3rem'}}>
+        <button onClick={() => setState({width: 100})}>Increase bar size</button>
+        <button onClick={() => setState({width: 3})}>Decrease bar size</button>
     </div>
-  );
+    </>
 }
 
-export default App;
+export default Bar;
+
+
+//initialy
+// const initializeState = () => ({
+//   width: 100
+// })
+// // ✅ note how the value returned from the fn above overrides initialState below: 
+// const initialState = { width: 15 }
+// const reducer = (state, action) => {
+//   switch (action) {
+//     case 'plus':
+//       return { width: state.width + 15 }
+//     case 'minus':
+//       return { width: Math.max(state.width - 15, 2) }
+//     default:
+//       throw new Error("what's going on?" )
+//   }
+// }
+
+// const Bar = () => {
+//   const [state, dispatch] = useReducer(reducer, initialState, initializeState)
+//   return <>
+//     <div style={{ background: 'teal', height: '30px', width: state.width }}></div>
+//     <div style={{marginTop: '3rem'}}>
+//         <button onClick={() => dispatch('plus')}>Increase bar size</button>
+//         <button onClick={() => dispatch('minus')}>Decrease bar size</button>
+//     </div>
+//     </>
+// }
+// export default Bar;
+
